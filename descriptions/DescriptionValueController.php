6d811c776df0258298527cf77b6dd13ad7b6ba42
dostric <dinko.ostric@gmail.com>
2014-03-24 23:23:09 +0100

    updated descriptions part.

--- descriptions/DescriptionValueController.php
+++ descriptions/DescriptionValueController.php
@@ -1,34 +1,63 @@
 <?php
 
 
+/**
+ * Class DescriptionValueController
+ *
+ * @method DescriptionValue_Model get($id, $default = null)
+ * @method DescriptionValue_Model[] all()
+ *
+ */
 class DescriptionValueController extends CollectionBase implements ControllerSearchableInterface {
 
 
+    protected $parent;
 
-    public function __construct() {
+
+    protected $found;
+
+
+    /**
+     * @param DescriptionItem|null $parent
+     * @param DescriptionValue_Model[] $values
+     */
+    public function __construct($parent = null, array $values = null) {
+
+        $this->parent = $parent;
+
+        if ($values) {
+            parent::__construct($values);
+        }
 
     }
 
 
-    public static function make() {
+    public static function make($parent = null, array $values = null) {
+        return new static($parent, $values);
+    }
+
+
+    public function find($ss) {
+
+        return $this;
+    }
+
+
+    public function load($ss = null) {
+
+        // if we have the ss load this items
+        // if we have the found list load the list
+
+        return $this;
 
     }
 
 
-    public function find() {
-
+    public function getList() {
+        return $this->found;
     }
-
-
-    public function load() {
-
-    }
-
-
-
-
-
 
 
 }
+
 
